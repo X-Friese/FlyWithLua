054642d05d324c4ab6abc9febb02af922f184c02
sparker256 <bgood@velocity.net>
2015-08-25 21:49:05 -0400

    Updated Mac and Linux builds to use the latest Luajit 2.0.4 version

--- FlyWithLua.cpp
+++ FlyWithLua.cpp
@@ -2,8 +2,8 @@
 //  FlyWithLua Plugin for X-Plane 10 (and X-Plane 9)
 // --------------------------------------------------
 
-#define PLUGIN_VERSION "2.4.1 nightly build " __DATE__ " " __TIME__
-// #define PLUGIN_VERSION "2.4.0 stable build " __DATE__ " " __TIME__
+//#define PLUGIN_VERSION "2.4.1 nightly build " __DATE__ " " __TIME__
+#define PLUGIN_VERSION "2.4.1 stable build " __DATE__ " " __TIME__
 #define PLUGIN_NAME "FlyWithLua"
 #define PLUGIN_DESCRIPTION "Use Lua to manipulate DataRefs and control HID devices."
 
