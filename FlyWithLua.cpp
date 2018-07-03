37ced4019f1151ecb63c830520adeebfd20298ab
Folke Will <folko@solhost.org>
2018-07-03 15:19:07 +0200

    Increase version number

--- FlyWithLua.cpp
+++ FlyWithLua.cpp
@@ -2,7 +2,7 @@
 //  FlyWithLua Plugin for X-Plane 11
 // ----------------------------------
 
-#define PLUGIN_VERSION "2.6.8 beta 1 build " __DATE__ " " __TIME__
+#define PLUGIN_VERSION "2.6.8 beta 5 build " __DATE__ " " __TIME__
 
 #if CREATECOMPLETEEDITION
 
