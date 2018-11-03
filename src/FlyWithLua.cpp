bb669aafa096d2ee675e51768f38fdd0a362295e
Nevkontakte <aleks@nevkontakte.com>
2018-11-03 23:46:44 +0000

    Remove DllMain function, which is not needed with MinGW (at least).

--- src/FlyWithLua.cpp
+++ src/FlyWithLua.cpp
@@ -172,21 +172,6 @@
 
 #if IBM
 #include <windows.h>
-BOOL APIENTRY DllMain( HANDLE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return true;
-}
 #include <unistd.h>
 #endif
 
