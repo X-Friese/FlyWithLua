4aec2b91b982c760b04b0ae96819af6f9ac91ddd
sparker256 <bgood@velocity.net>
2019-03-11 07:57:27 -0400

    Start using iniReader for preference file.
    Improved structure. Cleaned up un needed code.
    
    Updated version number to 2.7.16. Small cleanup.
    
    Updated manual about the preference file.

---
+++ src/third_party/iniReader/inireader.cpp
@@ -0,0 +1,130 @@
+/*
+ * File:   iniReader.c++
+ * Author: benmaynard
+ *
+ * Created on August 26, 2010, 2:49 PM
+ */
+
+#include "inireader.h"
+#include <cctype>
+#include <functional>
+#include <map>
+#include <stdexcept>
+#include <utility>
+
+using namespace std;
+
+std::map<std::string, std::string> configMap;
+
+void parseIniFile(char *fileName) {
+  std::string optionValue;
+  std::ifstream infile;
+  infile.open(fileName);
+
+  // Does the file exist?
+  if (!infile.is_open()) {
+    return;
+  }
+
+  std::string key;
+
+  while (!infile.eof()) // To get you all the lines.
+  {
+    getline(infile, optionValue); // Saves the line in STRING.
+
+    // Is the option a comment
+    if (optionValue.substr(0, 1) == "#") {
+      continue;
+    }
+
+    key = parseOptionName(optionValue);
+
+    if (key.length() > 0) {
+      configMap[key] = parseOptionValue(optionValue);
+    }
+  }
+
+  infile.close();
+}
+
+void cleanupIniReader() { configMap.clear(); }
+
+std::string getOptionToString(std::string key) {
+  try {
+    return configMap.at(key);
+  } catch (const std::out_of_range &oor) {
+    return "";
+  }
+}
+
+const char *getOptionToChar(std::string key) {
+  try {
+    return configMap.at(key).c_str();
+  } catch (const std::out_of_range &oor) {
+    return "";
+  }
+}
+
+int getOptionToInt(std::string key) {
+  try {
+    return atoi(configMap.at(key).c_str());
+  } catch (const std::out_of_range &oor) {
+    return 0;
+  }
+}
+
+void readOptionAsInt(std::string key, int *value) {
+  try {
+    *value = atoi(configMap.at(key).c_str());
+  } catch (const std::out_of_range &oor) {
+  }
+}
+
+std::string parseOptionName(std::string value) {
+  size_t found;
+
+  found = value.find('=');
+
+  if (found > 100) {
+    return "";
+  }
+
+  std::string key = value.substr(0, (found - 1));
+  key = trim(key);
+
+  return key;
+}
+
+std::string parseOptionValue(std::string value) {
+  size_t found;
+
+  found = value.find('=');
+
+  if (found > 100) {
+    return "";
+  }
+
+  std::string keyValue = value.substr((found + 1));
+  keyValue = trim(keyValue);
+
+  return keyValue;
+}
+
+std::string trim(std::string s) { return ltrim(rtrim(std::move(s))); }
+
+// trim from start
+std::string ltrim(std::string s) {
+  s.erase(s.begin(),
+          std::find_if(s.begin(), s.end(),
+                       std::not1(std::ptr_fun<int, int>(std::isspace))));
+  return s;
+}
+
+// trim from end
+std::string rtrim(std::string s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       std::not1(std::ptr_fun<int, int>(std::isspace)))
+              .base(),
+          s.end());
+  return s;
+}
